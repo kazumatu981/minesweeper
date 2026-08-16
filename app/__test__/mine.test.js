d948e961bd861413841b44afd65b39afafc1ed23
kazumatu981 <kazumatu981@gmail.com>
2026-08-16 15:15:03 +0900

    just backup.

---
+++ app/__test__/mine.test.js
@@ -0,0 +1,18 @@
+import { Mine, MINE_STATES } from '../parts/mine.js';
+
+// ドキュメントをすべて読み込んだら onPageLoad関数を呼び出す
+document.addEventListener('DOMContentLoaded', onPageLoad);
+
+/**
+ * ページが読み込まれたときに呼び出される関数
+ */
+function onPageLoad() {
+    const testPanel1 = document.getElementById('test001');
+    const mine0_0 = new Mine(0, 0);
+    const mine0_1 = new Mine(0, 1);
+    testPanel1.appendChild(mine0_0.element);
+    testPanel1.appendChild(mine0_1.element);
+
+    mine0_0.state = MINE_STATES.closed;
+    mine0_1.state = MINE_STATES.opened;
+}
--- app/parts/mine.js
+++ app/parts/mine.js
@@ -46,11 +46,74 @@ export const MINE_EVENTS = {
 };
 //#endregion
 
+//#region mine face and class
+const MINE_CLASS_PREFIX = '__mine_';
+const MINE_CLASS_BASE = '__mine_base';
+const MINE_STATE_CLASSES = {
+    /**
+     * MINEが閉じている
+     */
+    [MINE_STATE_CLOSED]: {
+        text: 'C',
+        class: `${MINE_CLASS_PREFIX}${MINE_STATE_CLOSED}`,
+    },
+    /**
+     * MINEの評価済みである
+     */
+    [MINE_STATE_ESTIMATED]: {
+        text: 'E',
+        class: `${MINE_CLASS_PREFIX}${MINE_STATE_ESTIMATED}`,
+    },
+    /**
+     * BOMBだと思ってフラグを付けている状態
+     */
+    [MINE_STATE_MUST_BE_BOMB]: {
+        text: 'F',
+        class: `${MINE_CLASS_PREFIX}${MINE_STATE_MUST_BE_BOMB}`,
+    },
+    /**
+     * BOMBかもしれない思ってフラグを付けている状態
+     */
+    [MINE_STATE_MAY_BE_BOMB]: {
+        text: '?',
+        class: `${MINE_CLASS_PREFIX}${MINE_STATE_MUST_BE_BOMB}`,
+    },
+    /**
+     * MINEが開いている
+     */
+    [MINE_STATE_OPENED]: {
+        text: 'O',
+        class: `${MINE_CLASS_PREFIX}${MINE_STATE_OPENED}`,
+    },
+};
+const MINE_IS_BOMB_CLASS = {
+    text: 'B',
+    class: `${MINE_CLASS_PREFIX}bomb`,
+};
+const MINE_NEIGHBOR_CLASSES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    .map((i) => {
+        return {
+            text: i.toString(),
+            class: `${MINE_CLASS_PREFIX}neighbor${i}`,
+        };
+    })
+    .reduce((prev, current) => {
+        prev[current.text] = current;
+        return prev;
+    }, {});
+
+const ALL_CLASSES = [
+    MINE_IS_BOMB_CLASS,
+    ...Object.values(MINE_STATE_CLASSES),
+    ...Object.values(MINE_NEIGHBOR_CLASSES),
+];
+//#endregion
+
 export class Mine extends EventHandler {
     _element;
     _rowId;
     _colId;
-    _state = MINE_STATES.closed;
+    _state;
     _neighborBombCount = 0;
     _isEstimated = false;
     _isBomb = false;
@@ -62,6 +125,10 @@ export class Mine extends EventHandler {
 
         this._element = document.createElement('div');
         this._element.id = this.id;
+        this._element.classList.add(MINE_CLASS_BASE);
+        this._registerEvent();
+
+        this.state = MINE_STATE_CLOSED;
     }
 
     get state() {
@@ -138,11 +205,7 @@ export class Mine extends EventHandler {
         });
 
         // 爆弾判定の登録
-        this.on(MINE_EVENT_STATE_CHANGE, () => {
-            if (this.isBomb) {
-                this.fire(MINE_EVENT_BOOM, this);
-            }
-        });
+        this.on(MINE_EVENT_STATE_CHANGE, this._checkBomb.bind(this));
 
         // スタイル変更イベントの登録
         this.on(MINE_EVENT_STATE_CHANGE, this._adjustFaceAndStyle.bind(this));
@@ -173,12 +236,36 @@ export class Mine extends EventHandler {
         this.state = nextState;
     }
 
+    _checkBomb() {
+        if (this.state === MINE_STATE_OPENED && this.isBomb) {
+            this.fire(MINE_EVENT_BOOM, this);
+        }
+    }
     /**
      * テキストとスタイルの調整
      */
     _adjustFaceAndStyle() {
-        // テキストの初期化
         // スタイルの初期化
+        ALL_CLASSES.forEach((item) => {
+            this._element.classList.remove(item.class);
+        });
+
+        this.element.textContent = this._currentStateClass.text;
+        this.element.classList.add(this._currentStateClass.class);
+        if (this.state === MINE_STATE_OPENED) {
+            if (this.isBomb) {
+                this.element.textContent = MINE_IS_BOMB_CLASS.text;
+                this.element.classList.add(MINE_IS_BOMB_CLASS.class);
+            }
+        } else if (this.state === MINE_STATE_ESTIMATED) {
+            const currentEstimated = MINE_NEIGHBOR_CLASSES[this.neighborCount];
+            this.element.textContent = currentEstimated.text;
+            this.element.classList.add(currentEstimated.class);
+        }
+    }
+
+    get _currentStateClass() {
+        return MINE_STATE_CLASSES[this.state];
     }
 }
 
