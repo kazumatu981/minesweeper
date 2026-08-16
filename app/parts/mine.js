77f0c98eee248e770bd3f6433d82ff6f8e1ec30f
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2026-08-16 10:50:58 +0900

    just backup.

--- app/parts/mine.js
+++ app/parts/mine.js
@@ -1,43 +1,50 @@
 import { EventHandler } from '../common/event-handler.js';
 
+/**
+ * MINEオブジェクトのIDプレフィックス
+ */
 const MINE_ID_PREFIX = '___mine___';
 
+//#region mine states
 const MINE_STATE_CLOSED = 'closed';
-const MIME_STATE_ESTIMATED = 'estimated';
-const MIME_STATE_MUST_BE_BOMB = 'must-be';
-const MIME_STATE_MAY_BE_BOMB = 'may-be';
+const MINE_STATE_ESTIMATED = 'estimated';
+const MINE_STATE_MUST_BE_BOMB = 'must-be';
+const MINE_STATE_MAY_BE_BOMB = 'may-be';
 const MINE_STATE_OPENED = 'opened';
 
 export const MINE_STATES = {
     /**
-     * Mimeが閉じている
+     * MINEが閉じている
      */
     [MINE_STATE_CLOSED]: MINE_STATE_CLOSED,
     /**
-     * Mimeの評価済みである
+     * MINEの評価済みである
      */
-    [MIME_STATE_ESTIMATED]: MIME_STATE_ESTIMATED,
+    [MINE_STATE_ESTIMATED]: MINE_STATE_ESTIMATED,
     /**
      * BOMBだと思ってフラグを付けている状態
      */
-    [MIME_STATE_MUST_BE_BOMB]: MIME_STATE_MUST_BE_BOMB,
+    [MINE_STATE_MUST_BE_BOMB]: MINE_STATE_MUST_BE_BOMB,
     /**
      * BOMBかもしれない思ってフラグを付けている状態
      */
-    [MIME_STATE_MAY_BE_BOMB]: MIME_STATE_MAY_BE_BOMB,
+    [MINE_STATE_MAY_BE_BOMB]: MINE_STATE_MAY_BE_BOMB,
     /**
-     * Mimeが開いている
+     * MINEが開いている
      */
     [MINE_STATE_OPENED]: MINE_STATE_OPENED,
 };
+//#endregion
 
-const MIME_EVENT_STATE_CHANGE = 'state-change';
-const MIME_EVENT_BOOM = 'boon';
+//#region mine events
+const MINE_EVENT_STATE_CHANGE = 'state-change';
+const MINE_EVENT_BOOM = 'boon';
 
-export const MIME_EVENTS = {
-    [MIME_EVENT_STATE_CHANGE]: MIME_EVENT_STATE_CHANGE,
-    [MIME_EVENT_BOOM]: MIME_EVENT_BOOM,
+export const MINE_EVENTS = {
+    [MINE_EVENT_STATE_CHANGE]: MINE_EVENT_STATE_CHANGE,
+    [MINE_EVENT_BOOM]: MINE_EVENT_BOOM,
 };
+//#endregion
 
 export class Mine extends EventHandler {
     _element;
@@ -61,8 +68,10 @@ export class Mine extends EventHandler {
         return this._state;
     }
     set state(value) {
-        this._state = value;
-        this.on(MIME_EVENT_STATE_CHANGE, this);
+        if (this._state !== value) {
+            this._state = value;
+            this.fire(MINE_EVENT_STATE_CHANGE, this);
+        }
     }
 
     /**
@@ -119,6 +128,7 @@ export class Mine extends EventHandler {
     }
 
     _registerEvent() {
+        // DOM イベントの登録
         this._element.addEventListener('click', () => {
             this._onLeftClick();
         });
@@ -126,18 +136,59 @@ export class Mine extends EventHandler {
             domElement.preventDefault();
             this._onRightClick();
         });
+
+        // 爆弾判定の登録
+        this.on(MINE_EVENT_STATE_CHANGE, () => {
+            if (this.isBomb) {
+                this.fire(MINE_EVENT_BOOM, this);
+            }
+        });
+
+        // スタイル変更イベントの登録
+        this.on(MINE_EVENT_STATE_CHANGE, this._adjustFaceAndStyle.bind(this));
     }
 
     /**
      * 左クリックのイベントハンドラ
      */
-    _onLeftClick() {}
+    _onLeftClick() {
+        if (
+            [
+                MINE_STATE_CLOSED,
+                MINE_STATE_MAY_BE_BOMB,
+                MINE_STATE_ESTIMATED,
+            ].includes(this.state)
+        ) {
+            this.state = MINE_STATE_OPENED;
+        }
+    }
     /**
      * 右クリックの
      */
-    _onRightClick() {}
+    _onRightClick() {
+        let nextState = R_CLICK_STATE_MAP[this.state];
+        if (nextState === MINE_STATE_CLOSED && this.isEstimated) {
+            nextState = MINE_STATE_ESTIMATED;
+        }
+        this.state = nextState;
+    }
+
+    /**
+     * テキストとスタイルの調整
+     */
+    _adjustFaceAndStyle() {
+        // テキストの初期化
+        // スタイルの初期化
+    }
 }
 
 export function _formatId(rowId, colId) {
     return `${MINE_ID_PREFIX}${rowId}-${colId}`;
 }
+
+const R_CLICK_STATE_MAP = {
+    [MINE_STATE_CLOSED]: MINE_STATE_MUST_BE_BOMB,
+    [MINE_STATE_MUST_BE_BOMB]: MINE_STATE_MAY_BE_BOMB,
+    [MINE_STATE_MAY_BE_BOMB]: MINE_STATE_CLOSED,
+    [MINE_STATE_OPENED]: MINE_STATE_OPENED,
+};
